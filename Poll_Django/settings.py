7038888aedb4a07e87c766259e9564bf26644566
Kiran Prajapati <kir.prz@gmail.com>
2018-11-27 16:42:21 +0545

    Minor changes

--- Poll_Django/settings.py
+++ Poll_Django/settings.py
@@ -135,6 +135,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        # 'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
+}
+
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
+    'localhost:8081',
 )
