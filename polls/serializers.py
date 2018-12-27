8dcc3d95b5c726557420dfe289b4356e8c481eab
Kiran Prajapati <kir.prz@gmail.com>
2018-12-27 15:54:29 +0545

    create and update serializer edited in nestedpolls

--- polls/serializers.py
+++ polls/serializers.py
@@ -16,21 +16,50 @@ class OptionSerializer(serializers.ModelSerializer):
         model = Option
         fields = '__all__'
 
+
+class OptionMinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Option
+        fields = (
+            'id',
+            'description'
+        )
+
+
 class NestedQuestionSerializer(serializers.ModelSerializer):
-    options = OptionSerializer(many=True)
+    options = OptionMinSerializer(many=True)
 
     class Meta:
         model = Question
         fields = (
             'id',
             'question',
-            'poll',
             'options'
         )
 
+
+
 class NestedPollSerializer(serializers.ModelSerializer):
     questions = NestedQuestionSerializer(many=True)
+
+    def create(self, validated_data):
+        print('data', validated_data)
+        poll = Poll.objects.create(poll_name=validated_data['poll_name'])
+        for each_question in validated_data['questions']:
+            question = Question.objects.create(poll_id=poll.id, question=each_question.get('question'))
+            for choices in each_question.get('options'):
+                choice = Option.objects.create(count=0, description=choices.get('description'), question_id=question.id)
         
+        return poll
+
+    def update(self, instance, validated_data):
+        for each_question in validated_data['questions']:
+            question = Question.objects.create(poll_id=instance.id, question=each_question.get('question'))
+            for choices in each_question.get('options'):
+                choice = Option.objects.create(count=0, description=choices.get('description'), question_id=question.id)
+
+        return instance
+    
     class Meta:
         model = Poll
         fields = (
