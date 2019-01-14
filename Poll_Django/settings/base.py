f6d97c6db70d2d4c734d5795e2e37cc4a839af9c
Kiran Prajapati <kir.prz@gmail.com>
2019-01-14 10:25:39 +0545

    Settings folder added

--- Poll_Django/settings/base.py
+++ Poll_Django/settings/base.py
@@ -90,17 +90,6 @@ WSGI_APPLICATION = 'Poll_Django.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'poll',
-        'USER': 'postgres',
-        'PASSWORD': 'kira',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
---
+++ Poll_Django/settings/development.py
@@ -0,0 +1,12 @@
+from .base import INSTALLED_APPS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'poll',
+        'USER': 'postgres',
+        'PASSWORD': 'kira',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
